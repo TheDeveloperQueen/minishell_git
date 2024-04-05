17e3641c3a50b0ca060cec33a267dd9317e95c57
Rita Vasques <rivasque@c2r10s1.42madrid.com>
2024-04-05 13:12:35 +0200

    exit_ok

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -6,11 +6,26 @@
 /*   By: rivasque <rivasque@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 19:31:32 by ritavasques       #+#    #+#             */
-/*   Updated: 2024/04/04 11:05:57 by rivasque         ###   ########.fr       */
+/*   Updated: 2024/04/05 13:01:34 by rivasque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+static void	exit_error_num(t_command *cmd, t_data *data)
+{
+	printf("ourShell: exit: %s: numeric argument required\n",
+		(char *)cmd->args->content);
+	data->exit_value = 255;
+	exit(EXIT_FAILURE);
+}
+
+static void	exit_error_size(t_data *data)
+{
+	printf("ourShell: exit: too many arguments\n");
+	data->exit_value = 1;
+	exit(EXIT_FAILURE);
+}
 
 int	ft_exit(t_command *cmd, t_data *data)
 {
@@ -19,17 +34,16 @@ int	ft_exit(t_command *cmd, t_data *data)
 	if (!cmd->args || !cmd->args->content)
 		exit(data->exit_value);
 	if (!ft_str_isdigit(cmd->args->content))
-	{
-		printf("ourShell: exit: %s: numeric argument required\n",
-			(char *)cmd->args->content);
-		data->exit_value = 255;
-		exit(EXIT_FAILURE);
-	}
+		exit_error_num(cmd, data);
 	else if (ft_lstsize(cmd->args) > 1)
 	{
-		printf("ourShell: exit: too many arguments\n");
-		data->exit_value = 1;
-		exit(EXIT_FAILURE);
+		if (!ft_str_isdigit(cmd->args->content))
+			exit_error_size(data);
+		else
+		{
+			printf("ourShell: exit: too many arguments\n");
+			return (1);
+		}
 	}
 	else
 	{
