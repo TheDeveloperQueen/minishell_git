a9c1e7cb57137b48d8cc0e886ddb409a89a3d902
acoto-gu <arleycoto94@gmail.com>
2024-04-08 16:50:46 +0200

    change: some exit codes

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: acoto-gu <acoto-gu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 19:31:32 by ritavasques       #+#    #+#             */
-/*   Updated: 2024/04/05 20:57:28 by acoto-gu         ###   ########.fr       */
+/*   Updated: 2024/04/08 16:38:44 by acoto-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,40 +16,28 @@ static void	exit_error_num(t_command *cmd, t_data *data)
 {
 	printf("%s: exit: %s: numeric argument required\n", data->shell_name,
 		(char *)cmd->args->content);
-	data->exit_value = 255;
-	exit(EXIT_FAILURE);
-}
-
-static void	exit_error_size(t_data *data)
-{
-	printf("%s: exit: too many arguments\n", data->shell_name);
-	data->exit_value = 1;
-	exit(EXIT_FAILURE);
+	exit(2);
 }
 
 int	ft_exit(t_command *cmd, t_data *data)
 {
+	int	exit_value;
+
 	printf("exit\n");
-	data->exit_value = 0;
 	if (!cmd->args || !cmd->args->content)
-		exit(data->exit_value);
+		exit(data->status);
 	if (!ft_str_isdigit(cmd->args->content))
 		exit_error_num(cmd, data);
 	else if (ft_lstsize(cmd->args) > 1)
 	{
-		if (!ft_str_isdigit(cmd->args->content))
-			exit_error_size(data);
-		else
-		{
-			printf("%s: exit: too many arguments\n", data->shell_name);
-			return (1);
-		}
+		printf("%s: exit: too many arguments\n", data->shell_name);
+		return (1);
 	}
 	else
 	{
-		data->exit_value = ft_atoi(cmd->args->content);
-		data->exit_value = (data->exit_value % 256);
-		exit(data->exit_value);
+		exit_value = ft_atoi(cmd->args->content);
+		exit_value = (exit_value % 256);
+		exit(exit_value);
 	}
 	return (0);
 }
