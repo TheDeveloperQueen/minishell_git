38966189dfef82390a2787d4a68c5597cb0d3d62
acoto-gu <arleycoto94@gmail.com>
2024-04-10 09:41:15 +0200

    Fix error: free(): invalid pointer in get_envp_3

--- src/builtins/aux_envp_2.c
+++ src/builtins/aux_envp_2.c
@@ -6,7 +6,7 @@
 /*   By: acoto-gu <acoto-gu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 09:08:03 by acoto-gu          #+#    #+#             */
-/*   Updated: 2024/04/10 09:16:33 by acoto-gu         ###   ########.fr       */
+/*   Updated: 2024/04/10 09:37:59 by acoto-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 void	get_envp_3(t_data *data)
 {
-	lst_add_back(&data->envp, lst_add_new("PWD", getcwd(NULL, 0)));
-	lst_add_back(&data->envp, lst_add_new("SHLVL", "1"));
-	lst_add_back(&data->envp, lst_add_new("_", "/usr/bin/env"));
+	lst_add_back(&data->envp, lst_add_new(ft_strdup("PWD"), getcwd(NULL, 0)));
+	lst_add_back(&data->envp, lst_add_new(ft_strdup("SHLVL"), ft_strdup("1")));
+	lst_add_back(&data->envp, lst_add_new(ft_strdup("_"),
+			ft_strdup("/usr/bin/env")));
 }
