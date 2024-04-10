bf85f67fdc576d3e61c6534d1dfb0c087d689cdc
acoto-gu <arleycoto94@gmail.com>
2024-04-10 09:18:11 +0200

    fix typo in function get_envp_3

--- src/builtins/aux_envp_2.c
+++ src/builtins/aux_envp_2.c
@@ -6,7 +6,7 @@
 /*   By: acoto-gu <acoto-gu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 09:08:03 by acoto-gu          #+#    #+#             */
-/*   Updated: 2024/04/10 09:08:52 by acoto-gu         ###   ########.fr       */
+/*   Updated: 2024/04/10 09:16:33 by acoto-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	get_envp_3(t_data *data)
 {
-	lst_add_back(&data->envp, lst_add_new("PWD=", getcwd(NULL, 0)));
+	lst_add_back(&data->envp, lst_add_new("PWD", getcwd(NULL, 0)));
 	lst_add_back(&data->envp, lst_add_new("SHLVL", "1"));
 	lst_add_back(&data->envp, lst_add_new("_", "/usr/bin/env"));
 }
