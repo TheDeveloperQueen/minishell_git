0da5090c29f9d4f9de0d38e0c7c7238942da3c8e
Rita Vasques <rivasque@c2r11s2.42madrid.com>
2024-04-11 16:12:45 +0200

    ft_cd

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: rivasque <rivasque@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 10:42:27 by ritavasques       #+#    #+#             */
-/*   Updated: 2024/04/11 12:40:38 by rivasque         ###   ########.fr       */
+/*   Updated: 2024/04/11 16:03:35 by rivasque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 
 static int	manage_path(t_data *data, char *old)
 {
+	char	*new_pwd;
+	
+	new_pwd = getcwd(NULL, 0);
 	if (!check_dup_env(data, "OLDPWD"))
-		lst_add_back(&data->envp, lst_add_new(ft_strdup("OLDPWD"), NULL));
-	check_envp(data, "OLDPWD", old);
+		lst_add_back(&data->envp, lst_add_new(ft_strdup("OLDPWD"), old));
+	else
+		check_envp(data, "OLDPWD", old);
 	if (!check_dup_env(data, "PWD"))
-		lst_add_back(&data->envp, lst_add_new(ft_strdup("PWD"), NULL));
-	check_envp(data, "PWD", getcwd(NULL, 0));
+		lst_add_back(&data->envp, lst_add_new(ft_strdup("PWD"), new_pwd));
+	else
+		check_envp(data, "PWD", new_pwd);
 	return (0);
 }
 
@@ -41,7 +46,7 @@ static int	path_home(t_data *data, t_cmd *cmd)
 	else
 		return (1);
 	manage_path(data, old);
-	return (0);
+	return (free(path), 0);
 }
 
 static int	path_old(t_data *data, t_cmd *cmd)
@@ -95,13 +100,16 @@ int	ft_cd(t_data *data, t_cmd *cmd)
 
 	if (check_args_size(cmd, 1))
 		return (1);
-	old = getcwd(NULL, 0);
 	if (!cmd->args || ft_strcmp(cmd->args->content, "~") == 0)
 		return (path_home(data, cmd));
 	else if (ft_strcmp(cmd->args->content, "-") == 0)
 		return (path_old(data, cmd));
 	else if (opendir((char *)cmd->args->content))
+	{
+		old = getcwd(NULL, 0);
 		chdir((char *)cmd->args->content);
+		manage_path(data, old);
+	}
 	else
 	{
 		msg = ft_strjoin(cmd->name, ": ");
@@ -109,6 +117,5 @@ int	ft_cd(t_data *data, t_cmd *cmd)
 		print_error_msg(msg2, "No such file or directory");
 		return (free(msg), free(msg2), 1);
 	}
-	manage_path(data, old);
 	return (0);
 }
